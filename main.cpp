67d97c69d318140b476ee5b420c010f6e73c7453
TheClonker <philip.gisella@googlemail.com>
2012-10-27 16:10:13 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: pgisella
+ *
+ * Created on 27. Oktober 2012, 15:57
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
